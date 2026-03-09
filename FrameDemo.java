efecd14eb235756235b21d7a1046f147c051e9b5
shivaraman <shevaraman2007@gmail.com>
2026-03-09 12:19:37 +0530

    Add files via upload

---
+++ FrameDemo.java
@@ -0,0 +1,11 @@
+import java.awt.*;  
+class FrameDemo {  
+
+    public static void main(String args[]) {   
+        Frame f = new Frame();   
+
+        f.setTitle("Shivaraman BCA"); 
+        f.setSize(500, 500);           
+        f.setVisible(true);           
+    }
+}
